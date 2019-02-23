5873eb247aa24b3cb63adc6ad05bdaa49da10ead
Rose <rose@RoseMacBook.local>
2019-02-24 03:43:57 +0900

    updated test.py

--- test.py
+++ test.py
@@ -2,10 +2,9 @@ import vlive
 
 
 def main():
-    channel_seq = 128 #GFRIEND:128, Red Velvet:548
-    [print('%s, %s, %s, %s, %s'%(i['video_seq'], i['time'], i['timestamp'], i['channel'], i['title'])) for i in vlive.channel(channel_seq, allow_other_channel=False, allow_mini_replay=False) if '어디' in i['title']]
-    video_list = vlive.channel(548, limit=1)
-    print(vlive.embed_code(video_list.video_list[0]['video_seq']))
+    channel_seq = 548 #GFRIEND:128, Red Velvet:548
+    [print('%s, %s, %s, %s, %s'%(i['video_seq'], i['time'], i['timestamp'], i['channel'], i['title'])) for i in vlive.channel(channel_seq, allow_other_channel=False, allow_mini_replay=False)]
+    [print(vlive.embed_code(i['video_seq'])) for i in vlive.channel(128) if '여자친구, 어디 감수광?!' in i['title']]
 
 
 if __name__ == '__main__':
