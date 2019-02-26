2c90efcc19bb7b02b23b3376aee6e7eff77e7d0a
yoki <yoki@yokimac.local>
2019-02-27 01:54:30 +0900

    modify

--- vlive_api/timestamp.py
+++ vlive_api/timestamp.py
@@ -1,8 +1,7 @@
 import datetime
 
 
-class generate_timestamp():
-    def __new__(cls, time):
-        time_object = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-        timestamp = int(time_object.timestamp())
-        return timestamp
+def generate_timestamp(time):
+    time_object = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+    timestamp = int(time_object.timestamp())
+    return timestamp
