2181c1d78baa46a949f6856d54cd930b96ba3cd1
Rose <rose@RoseMacBook.local>
2019-02-26 20:40:41 +0900

    modify

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
 from setuptools import setup, find_packages
 
 
-setup(
-    name='vlive_api',
-    version="0.0.1",
-    description="API of VLIVE",
-    long_description="Scraping Tool of VLIVE(https://www.vlive.tv/)",
-    url='https://github.com/mcgela/vlive_scraper',
-    author='MC Gela',
-    license='MIT',
-    classifiers=[
-        "Topic :: Utilities"
-    ],
-    keywords='VLIVE',
-    install_requires=[
-        "requests"
-    ]
-)
+setup()
