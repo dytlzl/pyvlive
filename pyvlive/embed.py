a30a6e77c029524ba88422d104d9bead6adb8d9e
rose <rose@mcgela.work>
2019-04-03 12:31:08 +0900

    Objectify Video

--- pyvlive/embed.py
+++
@@ -1,5 +0,0 @@
-class Embed:
-    EMBED_CODE = '<iframe src=https://www.vlive.tv/embed/%s?autoPlay=false" frameborder="no" scrolling="no" marginwidth="0" marginheight="0" WIDTH="544" HEIGHT="306" allowfullscreen></iframe>'
-
-    def __new__(cls, video_seq):
-        return cls.EMBED_CODE%(video_seq)
--- pyvlive/timestamp.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-
-def generate_timestamp(time):
-    time_object = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-    timestamp = int(time_object.timestamp())
-    return timestamp
